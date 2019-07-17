d3e86d7432be11540d7e20e6598dd93003f609c9
zohar pfeffer <pfzohar@gmail.com>
2019-07-17 14:37:02 +0300

    added add city post route and get all cities get route

--- server/models/City.js
+++ server/models/City.js
@@ -3,8 +3,11 @@ const Schema = mongoose.Schema
 
 const citySchema = new Schema({
     name: {type: String, required: true},
-    updatedAt: {type: Date, required: true},
+    updatedAt: {type: Date, required: false},
     temp: {type: Number, required: true},
     condition: {type: String, required: true},
-    conditionPic: {type: String, required: true}
+    conditionPic: {type: String, required: false}
 })
+const City = mongoose.model('City', citySchema)
+
+module.exports = City
