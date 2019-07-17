6832ab5b4c2ff8d934307395882afb20321a44cc
zohar pfeffer <pfzohar@gmail.com>
2019-07-17 19:14:40 +0300

    added renderData to Renderer

--- dist/js/Renderer.js
+++ dist/js/Renderer.js
@@ -1,3 +1,14 @@
 class Renderer {
     
+
+    renderData(allCities){
+        this.useHandlebars('city', allCities, '#cities')
+    }
+    
+    useHandlebars(templateName, data, appendTo){
+        const src = $('#'+templateName+'-template').html()
+        const template = Handlebars.compile(src)
+        const newHtml = template(data)
+        $(appendTo).append(newHtml)
+    }
 }
--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,8 @@
+// TODO:
+// failcase all calls
+
+
+
 const logic = new Logic
 const renderer = new Renderer
 const apiManager = new APIManager
