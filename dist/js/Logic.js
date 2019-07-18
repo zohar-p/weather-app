ab34c39bce4b81d2ce33f0d991abdd7ce0f3b22c
zohar pfeffer <pfzohar@gmail.com>
2019-07-18 14:18:32 +0300

    prevent adding to cityData if city already exist

--- dist/js/Logic.js
+++ dist/js/Logic.js
@@ -13,14 +13,19 @@ class Logic {
 
     async getCityData(cityName){
         const fetchedCityData = await apiManager.fetchCityData(cityName)
-        this.cityData.push({
-            name: fetchedCityData.location.name,
-            updatedAt: fetchedCityData.current.last_updated,
-            temp: fetchedCityData.current.temp_c,
-            condition: fetchedCityData.current.condition.text,
-            conditionPic: fetchedCityData.current.condition.icon,
-            isSaved: false
-        })
+        const alreadyExist = this.cityData.find(c=> c.name == fetchedCityData.location.name)
+        if(alreadyExist){
+            // TODO: fail case for if exist
+        } else {
+            this.cityData.push({
+                name: fetchedCityData.location.name,
+                updatedAt: fetchedCityData.current.last_updated,
+                temp: fetchedCityData.current.temp_c,
+                condition: fetchedCityData.current.condition.text,
+                conditionPic: fetchedCityData.current.condition.icon,
+                isSaved: false
+            })
+        }
     }
 
     async saveCity(cityName){
